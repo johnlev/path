a10c8a4037357d9463fe54d371f80b949b2aa934
ans19 <aaron.n.svendsen.19@dartmouth.edu>
2017-05-29 09:55:14 -0800

    Path

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -7,22 +7,23 @@ extends Node2D
 func _ready():
 	randomize()
 #	get_node("Silk").set_process(false)
-	var backId=1#=randi()%5
+	var backId=0#=randi()%5
 	print(backId)
-	if (backId==0):
-		get_node("Background").set_texture(load("res://Textures/PlainBackground/constellation.png"))
-		get_node("Background").translate(Vector2(375,667))
-	if (backId==1):
-		get_node("Background").set_texture(load("res://Textures/PlainBackground/meadows.png"))
+	if (backId<4):
+		if (backId==0):
+			get_node("Background").set_texture(load("res://Textures/PlainBackground/constellation.png"))
+			get_node("Music").set_stream(load("res://Music/River.ogg"))
+		if (backId==1):
+			get_node("Background").set_texture(load("res://Textures/PlainBackground/meadows.png"))
+			get_node("Music").set_stream(load("res://Music/River.ogg"))
+		if (backId==2):
+			get_node("Background").set_texture(load("res://Textures/PlainBackground/Black.png"))
+			get_node("Music").set_stream(load("res://Music/River.ogg"))
+		if (backId==3):
+			get_node("Background").set_texture(load("res://Textures/PlainBackground/Clouds.png"))
+			get_node("Music").set_stream(load("res://Music/River.ogg"))
 		get_node("Background").translate(Vector2(375,667))
 		get_node("Opacity").play("Breathe")
-	if (backId==2):
-		get_node("Background").set_texture(load("res://Textures/PlainBackground/Black.png"))
-		get_node("Background").translate(Vector2(375,667))
-		get_node("Opacity").play("Breathe")
-	if (backId==3):
-		get_node("Background").set_texture(load("res://Textures/PlainBackground/Clouds.png"))
-		get_node("Background").translate(Vector2(375,667))
 	if (backId==4):
 		get_node("Breath").set_texture(load("res://Textures/PlainBackground/border.jpg"))
 		get_node("Breath").translate(Vector2(25,667))
@@ -32,6 +33,7 @@ func _ready():
 		
 	if (backId==5):
 		get_node("Silk").set_process(true)
+	get_node("Music").play()
 	# Called every time the node is added to the scene.
 	# Initialization here
 	
