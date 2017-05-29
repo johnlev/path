db05ffee4eab3242b4fdde68ddf3b0f20d378f98
ans19 <aaron.n.svendsen.19@dartmouth.edu>
2017-05-29 13:05:32 -0800

    Graphics

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -3,7 +3,8 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
+var dot
+var cursor
 func _ready():
 	randomize()
 #	get_node("Silk").set_process(false)
@@ -12,7 +13,7 @@ func _ready():
 	if (backId<4):
 		if (backId==0):
 			get_node("Background").set_texture(load("res://Textures/PlainBackground/constellation.png"))
-			get_node("Music").set_stream(load("res://Music/River.ogg"))
+			get_node("Music").set_stream(load("res://Music/Sleep.ogg"))
 		if (backId==1):
 			get_node("Background").set_texture(load("res://Textures/PlainBackground/meadows.png"))
 			get_node("Music").set_stream(load("res://Music/River.ogg"))
@@ -23,7 +24,7 @@ func _ready():
 			get_node("Background").set_texture(load("res://Textures/PlainBackground/Clouds.png"))
 			get_node("Music").set_stream(load("res://Music/River.ogg"))
 		get_node("Background").translate(Vector2(375,667))
-		get_node("Opacity").play("Breathe")
+#		get_node("Opacity").play("Breathe")
 	if (backId==4):
 		get_node("Breath").set_texture(load("res://Textures/PlainBackground/border.jpg"))
 		get_node("Breath").translate(Vector2(25,667))
@@ -34,6 +35,15 @@ func _ready():
 	if (backId==5):
 		get_node("Silk").set_process(true)
 	get_node("Music").play()
+	cursor="res://Textures/Dots/sun.png"
+	dot = "res://Textures/Dots/star.png"
+	get_node("Path2D/Cursor/Path").set_texture(load(cursor))
+	get_node("Path2D/dot").set_texture(load(dot))
+	get_node("Path2D/dot1").set_texture(load(dot))
+	get_node("Path2D/dot2").set_texture(load(dot))
+	get_node("Path2D/dot3").set_texture(load(dot))
+	get_node("Path2D/dot4").set_texture(load(dot))
+	get_node("Path2D/dot5").set_texture(load(dot))
 	# Called every time the node is added to the scene.
 	# Initialization here
 	
